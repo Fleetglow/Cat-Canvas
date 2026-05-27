72192e7b991074ee4d4dbb59c96cfe4828901e48
Admin <47961924@qq.com>
2026-05-27 13:48:21 +0800

    更新 run.bat：bundled Python 升级到 3.14，优先使用本地 python\ 目录

--- run.bat
+++ run.bat
@@ -2,9 +2,18 @@
 cd /d "%~dp0"
 
 set "PYEXE=%~dp0python\python.exe"
-if not exist "%PYEXE%" set "PYEXE=python"
+if not exist "%PYEXE%" (
+    REM 备用：如果 bundled Python 不存在，找系统 Python 3.14/3.13/3.12/3.11
+    for %%v in (314 313 312 311) do (
+        if not defined PYEXE (
+            where python%%v >nul 2>&1 && set "PYEXE=python%%v"
+        )
+    )
+    if not defined PYEXE set "PYEXE=python"
+)
 
 echo Starting ComfyUI-API-Modelscope...
+echo Using Python: %PYEXE%
 echo Visit: http://127.0.0.1:3000/
 echo Press Ctrl+C to stop.
 echo.
